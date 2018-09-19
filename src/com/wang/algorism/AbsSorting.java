59633ba8bbe10609c6c769c91337a4057becd24d
alpena <wzqcn@live.com>
2018-09-19 22:55:07 +0800

    commit

--- src/com/wang/algorism/AbsSorting.java
+++ src/com/wang/algorism/AbsSorting.java
@@ -26,17 +26,19 @@ public class AbsSorting {
 
     }
 
-    /* function to sort arr using shellSort */
+    /**
+     * 希尔排序
+     * */
     private static int shellSort(int arr[]) {
-        int n = arr.length;
+        int len = arr.length;
 
         // Start with a big gap, then reduce the gap
-        for (int gap = n / 2; gap > 0; gap /= 2) {
+        for (int gap = len / 2; gap > 0; gap /= 2) {
             // Do a gapped insertion sort for this gap size.
             // The first gap elements a[0..gap-1] are already
             // in gapped order keep adding one more element
             // until the entire array is gap sorted
-            for (int i = gap; i < n; i += 1) {
+            for (int i = gap; i < len; i += 1) {
                 // add a[i] to the elements that have been gap
                 // sorted save a[i] in temp and make a hole at
                 // position i
@@ -45,8 +47,9 @@ public class AbsSorting {
                 // shift earlier gap-sorted elements up until
                 // the correct location for a[i] is found
                 int j;
-                for (j = i; j >= gap && arr[j - gap] > temp; j -= gap)
+                for (j = i; j >= gap && arr[j - gap] > temp; j -= gap) {
                     arr[j] = arr[j - gap];
+                }
 
                 // put temp (the original a[i]) in its correct
                 // location
