96bbc5da267ba8803f6196b0c7d0a7486d945008
alpena <wzqcn@live.com>
2018-10-09 17:51:16 +0800

    commit

--- src/com/wang/reflect/proxy/jdkproxy/GirlProxyInvocationHandler.java
+++ src/com/wang/reflect/proxy/jdkproxy/GirlProxyInvocationHandler.java
@@ -4,11 +4,14 @@ import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
-public class GirlProxy implements InvocationHandler {
+public class GirlProxyInvocationHandler implements InvocationHandler {
+
     private Girl girl;
-    public GirlProxy(Girl girl){
+
+    public GirlProxyInvocationHandler(Girl girl){
         this.girl = girl;
     }
+
     @Override
     public Object invoke(Object proxy, Method method, Object[] args) throws InvocationTargetException, IllegalAccessException {
         // 执行目标对象的方法
--- src/com/wang/reflect/proxy/jdkproxy/Main.java
+++ src/com/wang/reflect/proxy/jdkproxy/Main.java
@@ -5,8 +5,8 @@ import java.lang.reflect.Proxy;
 public class Main {
     public static void main(String[] args){
         Girl girl = new Girl();
-        GirlProxy girlProxy = new GirlProxy(girl);
-        MakeTrouble a = (MakeTrouble)Proxy.newProxyInstance(Thread.currentThread().getContextClassLoader(), Girl.class.getInterfaces(), girlProxy);
+        GirlProxyInvocationHandler girlProxyInvocationHandler = new GirlProxyInvocationHandler(girl);
+        MakeTrouble a = (MakeTrouble)Proxy.newProxyInstance(Thread.currentThread().getContextClassLoader(), Girl.class.getInterfaces(), girlProxyInvocationHandler);
         a.makeTrouble();
     }
 }
